e90afba50b5267df82656d8d23712bc82c4674c4
Douglas Bello <dgbello.dev@hotmail.com>
2024-09-05 16:22:58 -0300

    changes: renaming backend

---
+++ backend/src/main/java/com/gamersclubfinder/gamersclubfinder/GamersclubfinderApplication.java
@@ -0,0 +1,13 @@
+package com.gamersclubfinder.gamersclubfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamersclubfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamersclubfinderApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gamersclubfinder/gamersclubfinder/GamersclubfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamersclubfinder.gamersclubfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamersclubfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
