32774a80cdcbdf8c9ba8e00f2a6d2a2d05de7a0f
Douglas Bello <dgbello.dev@hotmail.com>
2024-09-05 15:26:35 -0300

    init project

---
+++ scrapper/scrapper.py
@@ -0,0 +1,39 @@
+import time
+import requests
+from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+link_gc = "https://gamersclub.com.br/player/{player_id}"
+
+def fetch_player(player_id):
+    response = requests.get(link_gc.format(player_id=player_id))
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        steam_link = soup.find('a', class_="Button--steam")
+        if steam_link:
+            return steam_link['href']
+    return None
+
+def main():
+    start = time.time()
+    total_encontradas = 0
+    with ThreadPoolExecutor(max_workers=50) as executor:
+        # Mapeando as requisições para os IDs
+        futures = [executor.submit(fetch_player, n) for n in range(1, 2000)]
+        
+        for future in as_completed(futures):
+            steam_link = future.result()
+            if steam_link:
+                total_encontradas += 1
+                print(steam_link)
+    
+    print("-------------------------------------------------------------------------------------")
+    print(f"Total encontradas = {total_encontradas}")
+    print("-------------------------------------------------------------------------------------")
+    end = time.time()
+    length = end - start;
+
+    print("It took: ", length, " seconds")
+
+if __name__ == "__main__":
+    main()
