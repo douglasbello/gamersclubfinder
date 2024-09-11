3c3f7661dbe7efb928911baa7357218e8b00dcb4
Douglas Bello <dgbello.dev@hotmail.com>
2024-09-11 11:11:44 -0300

    fix: throwing NotFoundException where necessary

--- backend/src/main/java/com/gamersclubfinder/gamersclubfinder/controllers/players/search/IPlayerSearchController.java
+++ backend/src/main/java/com/gamersclubfinder/gamersclubfinder/controllers/players/search/IPlayerSearchController.java
@@ -2,8 +2,9 @@ package com.gamersclubfinder.gamersclubfinder.controllers.players.search;
 
 import com.gamersclubfinder.gamersclubfinder.dtos.players.PlayerResponse;
 import com.gamersclubfinder.gamersclubfinder.dtos.players.SearchPlayerRequest;
+import com.gamersclubfinder.gamersclubfinder.handler.exceptions.NotFoundException;
 import org.springframework.http.ResponseEntity;
 
 public interface IPlayerSearchController {
-    ResponseEntity<PlayerResponse> findBySteamId(SearchPlayerRequest steamId);
+    ResponseEntity<PlayerResponse> findBySteamId(SearchPlayerRequest steamId) throws NotFoundException;
 }
--- backend/src/main/java/com/gamersclubfinder/gamersclubfinder/controllers/players/search/impl/PlayerSearchControllerImpl.java
+++ backend/src/main/java/com/gamersclubfinder/gamersclubfinder/controllers/players/search/impl/PlayerSearchControllerImpl.java
@@ -3,6 +3,7 @@ package com.gamersclubfinder.gamersclubfinder.controllers.players.search.impl;
 import com.gamersclubfinder.gamersclubfinder.controllers.players.search.IPlayerSearchController;
 import com.gamersclubfinder.gamersclubfinder.dtos.players.PlayerResponse;
 import com.gamersclubfinder.gamersclubfinder.dtos.players.SearchPlayerRequest;
+import com.gamersclubfinder.gamersclubfinder.handler.exceptions.NotFoundException;
 import com.gamersclubfinder.gamersclubfinder.services.players.search.IPlayerSearchService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +18,7 @@ public class PlayerSearchControllerImpl implements IPlayerSearchController {
     }
 
     @GetMapping
-    public ResponseEntity<PlayerResponse> findBySteamId(@RequestParam SearchPlayerRequest steamId) {
+    public ResponseEntity<PlayerResponse> findBySteamId(@RequestParam SearchPlayerRequest steamId) throws NotFoundException {
         return ResponseEntity.ok().body(playerSearchService.findBySteamId(steamId.steamId()));
     }
 }
